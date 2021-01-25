8efef99cda2fb4f9b61d08b9f8a39ee43e0edc77
Robert Heedt <robert.heedt@gmail.com>
2021-01-25 21:52:36 +0100

    Keyboard hook test

--- reneo-squire/source/app.d
+++ reneo-squire/source/app.d
@@ -1,6 +1,30 @@
 import std.stdio;
+import core.runtime;
+import core.sys.windows.windows;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+HHOOK hHook;
+
+extern (Windows)
+LRESULT LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) nothrow {
+    auto msg_ptr = cast(LPKBDLLHOOKSTRUCT) lParam;
+    auto vk = msg_ptr.vkCode;
+    switch(wParam) {
+        case WM_KEYDOWN:
+        printf("Key down %x\n", vk);
+        break;
+        default:
+        break;
+    }
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+void main() {
+	printf("Started\n");
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, &LowLevelKeyboardProc, hInstance, 0);
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
